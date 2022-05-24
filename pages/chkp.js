6d57140410003bdcb69699942799f5f992e90969
StavaMoscjne <102909696+StavaMoscjne@users.noreply.github.com>
2022-05-24 19:57:28 +0100

    Add files via upload

---
+++ pages/chkp.js
@@ -0,0 +1,11 @@
+        var h = window.outerHeight;
+        var w = window.outerWidth;
+        if(h>550 && w>550){
+            window.location.replace('../index_home.html');
+        }
+        else{
+            sessionStorage.setItem('browser','mobile');
+        }
+function forceRedirect(){window.location.replace('../index_home.html')};
+var squint = document.getElementById('squint');
+squint.addEventListener('click',forceRedirect);
---
+++ pages/resp.js
@@ -0,0 +1,3 @@
+function forceRedirect(){window.location.replace('../index_home.html')};
+var squint = document.getElementById('squint');
+squint.addEventListener('click',forceRedirect);
